2a72235ffd4998deb8902475b891d57958fe25e9
masa-bot1 <bdyht515@yahoo.co.jp>
2022-02-15 20:32:06 +0900

    スネークが曲がれるようにする

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Navigation from './components/Navigation'
 import Field from './components/Field'
 import Button from './components/Button'
@@ -14,6 +14,34 @@ const GameStatus = Object.freeze({
   playing: 'playing',
   suspended: 'suspended',
   gameover: 'gameover'
+})
+
+export const Direction = Object.freeze({
+  up: 'up',
+  right: 'right',
+  left: 'left',
+  down: 'down'
+})
+
+const DirectionKeyCodeMap = Object.freeze({
+  37: Direction.left,
+  38: Direction.up,
+  39: Direction.right,
+  40: Direction.down,
+})
+
+const OppositeDirection = Object.freeze({
+  up: 'down',
+  right: 'left',
+  left: 'right',
+  down: 'up'
+})
+
+const Delta = Object.freeze({
+  up: { x: 0, y: -1 },
+  right: { x:  1, y: 0 },
+  left: { x: -1, y: 0 },
+  down: { x: 0, y: 1 },
 })
 
 let timer = undefined
@@ -41,6 +69,7 @@ function App() {
   const [fields, setFields] = useState(initialValues)
   const [position, setPosition] = useState()
   const [status, setStatus] = useState(GameStatus.init)
+  const [direction, setDirection] = useState(Direction.up)
   const [tick, setTick] = useState(0)
 
   useEffect(() => {
@@ -57,7 +86,7 @@ function App() {
     if (!position || status !== GameStatus.playing) {
       return
     }
-    const canContinue = goUp()
+    const canContinue = handleMoving()
     if (!canContinue) {
       setStatus(GameStatus.gameover)
     }
@@ -71,18 +100,46 @@ function App() {
     }, defaultInterval)
     setStatus(GameStatus.init)
     setPosition(initialPosition)
+    setDirection(Direction.up)
     setFields(initFields(35, initialPosition))
   }
 
-  const goUp = () => {
+  const onChangeDirection = useCallback((newDirection) => {
+    if (status !== GameStatus.playing) {
+      return direction
+    }
+    if (OppositeDirection[direction] === newDirection) {
+      return
+    }
+    setDirection(newDirection)
+  }, [direction ,status])
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      const newDirection = DirectionKeyCodeMap[e.keyCode];
+      if (!newDirection) {
+        return;
+      }
+
+      onChangeDirection(newDirection);
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown)
+  }, [onChangeDirection])
+
+  const handleMoving = () => {
     const { x, y } = position
-    const newPosition = { x, y: y -1 }
+    const delta = Delta[direction]
+    const newPosition = {
+      x: x + delta.x,
+      y: y + delta.y
+    }
     if (isCollision(fields.length, newPosition)) {
       unsubscribe()
       return false
     }
     fields[y][x] = ''
-    fields[newPosition.y][x] = 'snake'
+    fields[newPosition.y][newPosition.x] = 'snake'
     setPosition(newPosition)
     setFields(fields)
     return true
@@ -101,7 +158,7 @@ function App() {
       </main>
       <footer className="footer">
         <Button status={status} onStart={onStart} onRestart={onRestart} />
-        <ManipulationPanel />
+        <ManipulationPanel onChange={onChangeDirection} />
       </footer>
     </div>
   );
